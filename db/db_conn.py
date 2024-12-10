dd0815f47f767247bfa0c8d009d54d3733720ff9
Balogun Muhammad Awwal <balogunmuhammadawwal@gmail.com>
2024-12-10 21:29:31 +0100

    feat: add db connection logic

---
+++ db/db_conn.py
@@ -0,0 +1,17 @@
+from collections.abc import AsyncGenerator
+from sqlalchemy.ext.asyncio import AsyncSession , create_async_engine, async_sessionmaker
+from .model import Base
+
+DB_URL='sqlite+aiosqlite:///./iceedge.db'
+engine = create_async_engine(DB_URL) 
+async_session_maker = async_sessionmaker(engine, expire_on_commit=False)
+
+
+async def get_async_engine() -> AsyncGenerator[AsyncSession, None]: 
+    async with async_session_maker() as session: 
+        yield session
+
+
+async def create_all_tables(): 
+    async with engine.begin() as conn: 
+        await conn.run_sync(Base.metadata.create_all)
