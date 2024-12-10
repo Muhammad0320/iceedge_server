a1f53adaf0cc5efd6a8a6899a32dc7eadb59dd70
Balogun Muhammad Awwal <balogunmuhammadawwal@gmail.com>
2024-12-10 21:24:30 +0100

    chore: use proper naming convention

--- db/model.py
+++ db/model.py
@@ -1,146 +1,91 @@
-from pydantic import BaseModel, EmailStr, model_validator, Field
+from sqlalchemy.orm import Mapped, mapped_column, DeclarativeBase, relationship
+from sqlalchemy import DateTime, String, Text, Integer, Float, JSON, Enum, ForeignKey
 from datetime import datetime
-from enum import Enum 
-from typing_extensions import Self
+from typing import List
+class Base(DeclarativeBase):
+    pass 
 
-class Role(str, Enum): 
+class Role(Enum): 
     ADMIN = 'admin'
     CUSTOMER = 'customer' 
     DEVELOPER = 'developer'
 
-class Cat(str, Enum): 
+class Cat(Enum): 
     SHIIRT = "shirt"
     PANT = 'pant'
     CAP = 'cap'
     SHOE = 'shoe'
     SOCK = 'sock'
-    MASK = "mask"
 
+class Product(Base): 
+    __tablename__='products'
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
+    price: Mapped[float] = mapped_column(Float, nullable=False) 
+    discount: Mapped[float] = mapped_column(Float, default=0) 
+    name: Mapped[str] = mapped_column(String(255), nullable=False) 
+    description: Mapped[str] = mapped_column(Text, nullable=False) 
+    cat_id: Mapped[int] = mapped_column(ForeignKey('category.id'), nullable=False)
+    thumbnail: Mapped[str] = mapped_column(String(255), nullable=False)
+    gallery: Mapped[list[str]] = mapped_column(JSON, nullable=False) 
+    amt_left: Mapped[int] = mapped_column(Integer, nullable=False)  
+    avg_rating: Mapped[float] = mapped_column(Float, default=0) 
+    ratings_count: Mapped[int] = mapped_column(Float, default=0) 
+    created_at: Mapped[datetime] = mapped_column(DateTime, default=datetime.now) 
+    reviews: Mapped[List["Review"]] = relationship("Review", cascade='all, delete') 
+    
 
-class ProductBase(BaseModel): 
-    price: float = Field(..., ge=2999.99) 
-    discount: int = Field(..., ge=0, le=99) 
-    name: str 
-    description: str 
-    category: Cat 
-    thumbnail: str 
-    gallery: list[str] 
-    amt_left: int = Field(..., ge=1) 
-    avg_rating: float = Field(0, ge=1.0, le=5.0)
-    ratings_count: int = Field(0, ge=0)
-    created_at: datetime = Field(default_factory=datetime.now)
+class User(Base): 
+    __tablename__='users'
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True) 
+    firstname: Mapped[str] = mapped_column(String(255), nullable=False) 
+    lastname: Mapped[str] = mapped_column(String(255), nullable=False)  
+    email: Mapped[str] = mapped_column(String(255), nullable=False) 
+    password: Mapped[str] = mapped_column(String(255), nullable=False) 
+    role: Mapped[Role] = mapped_column(Enum(Role), default=Role.CUSTOMER) 
+    shipping_address: Mapped[str] = mapped_column(Text, nullable=False) 
+    created_at: Mapped[datetime] = mapped_column(DateTime, default=datetime.now) 
+    orders: Mapped[List["Order"]] = relationship("Order", cascade='all, delete')
+    
+
+class Review(Base): 
+    __tablename__ = 'reviews'
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True) 
+    content: Mapped[str] = mapped_column(Text, nullable=False)     
+    rating: Mapped[float] = mapped_column(Float, nullable=False, max=5.0, min=1.0) 
+    created_at: Mapped[datetime] = mapped_column(DateTime, default=datetime.now) 
+    product_id: Mapped[int] = mapped_column(ForeignKey('products.id'), nullable=False) 
+    product: Mapped["Product"] = relationship("Product", back_populates='reviews' ) 
      
 
-class ProductCreate(ProductBase): 
-    pass 
-
-class ProductRead(ProductBase): 
-    id: int 
-    reviews: list["ReviewRead"] = Field(None) 
-
-class ProductUpdate(BaseModel): 
-    discount: int = Field(None, ge=0, le=99) 
-    name: str = Field(None) 
-    description: str = Field(None) 
-    category: Cat = Field(None) 
-    thumbnail: str = Field(None) 
-    gallery: list[str] = Field(None) 
-    amt_left: int = Field(None, ge=1) 
-    avg_rating: float = Field(None, ge=1.0, le=5.0)
-    ratings_count: int = Field(None, ge=0)
-
-class UserBase(BaseModel): 
-    firstname: str 
-    lastname: str 
-    email: EmailStr 
-    password: str = Field(..., min_length=8) 
-    password_confirm: str 
-    shipping_address: str 
-    role: Role = Field(default=Role.CUSTOMER)  
-    created_at: datetime = Field(default_factory=datetime.now) 
-
-class UserCreate(UserBase): 
-    @model_validator(mode='after') 
-    def validate_password(self) -> Self:
-        if self.password != self.password_confirm: 
-            raise ValueError("Passwords must match!") 
-        return self
-
-class UserRead(UserBase): 
-    id: int 
-    orders: list["OrderRead"] = Field(None) 
+class Order(Base): 
+    __tablename__= "orders"
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True) 
+    total: Mapped[float] = mapped_column(Integer, nullable=False) 
+    quantity: Mapped[int] = mapped_column(Integer, default=1) 
+    shipping_fee: Mapped[float] = mapped_column(Float, nullable=False) 
+    shipping_address: Mapped[float] = mapped_column(Float, nullable=True) 
+    created_at: Mapped[datetime] = mapped_column(DateTime, default=datetime.now) 
+    customer_id: Mapped[int] = mapped_column(ForeignKey('user.id'), nullable=False) 
+    customer: Mapped["User"] = relationship("User", back_populates="orders") 
+    order_items: Mapped[List['Item']] = relationship("Item") 
     
+# class Cart(Base): 
+#     __tablename__= "carts"
+#     id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True) 
+#     total: Mapped[float] = mapped_column(Float, nullable=False) 
     
-class UserUpdate(BaseModel): 
-    firstname: str = Field(None)  
-    lastname: str = Field(None)
-    email: EmailStr = Field(None)
-    password: str = Field(None, min_length=8) 
-    password_confirm: str = Field(None)
-    shipping_address: str = Field(None)
+#     created_at: Mapped[datetime] = mapped_column(DateTime, default=datetime.now) 
 
-class ReviewBase(BaseModel): 
-    content: str 
-    rating: float = Field(0, ge=1.0, le=5.0)
-    created_at: datetime = Field(default_factory=datetime.now) 
-    product_id: int 
-    
-class ReviewCreate(ReviewBase): 
-    pass 
+class Category(Base): 
+    __tablename__= "categorys"
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True) 
+    name: Mapped[Cat] = mapped_column(Enum(Cat), nullable=False) 
 
-class ReviewRead(ReviewBase): 
-    id: int 
-
-class ReviewUpdate(BaseModel): 
-    content: str = Field(None)
-    rating: float = Field(None, ge=1.0, le=5.0)
-
-class OrderBase(BaseModel): 
-    total: float = Field(..., ge=4999.99) 
-    quantity: int = Field(..., gt=1) 
-    shipping_fee: float 
-    shipping_address: str 
-    created_at: datetime = Field(default_factory=datetime.now) 
-    customer_id: int 
-    order_items: list["ItemRead"]
-
-class OrderCreate(OrderBase): 
-    pass 
-
-class OrderRead(OrderBase): 
-    id: int 
-
-class OrderUpdate(BaseModel): 
-    total: float = Field(None, ge=4999.99) 
-    quantity: int = Field(None, gt=1) 
-    shipping_fee: float = Field(None)
-    shipping_address: str = Field(None)
-
-class CategoryBase(BaseModel): 
-    name: Cat 
-
-class CategoryCreate(CategoryBase): 
-    pass 
-
-class CategoryRead(CategoryBase): 
-    id: int 
-
-class CategoryUpdate(BaseModel): 
-    name: Cat = Field(None)
-
-
-class ItemBase(BaseModel): 
-    total: float = Field(..., ge=4999.99) 
-    quantity: int = Field(..., gt=1) 
-    product_id: int 
-    order_id: int 
-
-class ItemCreate(ItemBase): 
-    pass
-
-class ItemRead(ItemBase): 
-    id: int 
-
-class ItemUpdate(BaseModel): 
-    total: float = Field( None, ge=4999.99) 
-    quantity: int = Field (None,  gt=1) 
+class Item(Base): 
+    __tablename__= "items"
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True) 
+    total: Mapped[float] = mapped_column(Float, nullable=False ) 
+    quantity: Mapped[int] = mapped_column(Integer, default=1) 
+    product_id: Mapped[int] = mapped_column(ForeignKey('products.id'), nullable=False) 
+    product: Mapped[Product] = relationship("Product") 
+    order_id: Mapped[int] = mapped_column(ForeignKey('order.id'), nullable=True)
---
+++ db/schema.py
@@ -0,0 +1,146 @@
+from pydantic import BaseModel, EmailStr, model_validator, Field
+from datetime import datetime
+from enum import Enum 
+from typing_extensions import Self
+
+class Role(str, Enum): 
+    ADMIN = 'admin'
+    CUSTOMER = 'customer' 
+    DEVELOPER = 'developer'
+
+class Cat(str, Enum): 
+    SHIIRT = "shirt"
+    PANT = 'pant'
+    CAP = 'cap'
+    SHOE = 'shoe'
+    SOCK = 'sock'
+    MASK = "mask"
+
+
+class ProductBase(BaseModel): 
+    price: float = Field(..., ge=2999.99) 
+    discount: int = Field(..., ge=0, le=99) 
+    name: str 
+    description: str 
+    category: Cat 
+    thumbnail: str 
+    gallery: list[str] 
+    amt_left: int = Field(..., ge=1) 
+    avg_rating: float = Field(0, ge=1.0, le=5.0)
+    ratings_count: int = Field(0, ge=0)
+    created_at: datetime = Field(default_factory=datetime.now)
+     
+
+class ProductCreate(ProductBase): 
+    pass 
+
+class ProductRead(ProductBase): 
+    id: int 
+    reviews: list["ReviewRead"] = Field(None) 
+
+class ProductUpdate(BaseModel): 
+    discount: int = Field(None, ge=0, le=99) 
+    name: str = Field(None) 
+    description: str = Field(None) 
+    category: Cat = Field(None) 
+    thumbnail: str = Field(None) 
+    gallery: list[str] = Field(None) 
+    amt_left: int = Field(None, ge=1) 
+    avg_rating: float = Field(None, ge=1.0, le=5.0)
+    ratings_count: int = Field(None, ge=0)
+
+class UserBase(BaseModel): 
+    firstname: str 
+    lastname: str 
+    email: EmailStr 
+    password: str = Field(..., min_length=8) 
+    password_confirm: str 
+    shipping_address: str 
+    role: Role = Field(default=Role.CUSTOMER)  
+    created_at: datetime = Field(default_factory=datetime.now) 
+
+class UserCreate(UserBase): 
+    @model_validator(mode='after') 
+    def validate_password(self) -> Self:
+        if self.password != self.password_confirm: 
+            raise ValueError("Passwords must match!") 
+        return self
+
+class UserRead(UserBase): 
+    id: int 
+    orders: list["OrderRead"] = Field(None) 
+    
+    
+class UserUpdate(BaseModel): 
+    firstname: str = Field(None)  
+    lastname: str = Field(None)
+    email: EmailStr = Field(None)
+    password: str = Field(None, min_length=8) 
+    password_confirm: str = Field(None)
+    shipping_address: str = Field(None)
+
+class ReviewBase(BaseModel): 
+    content: str 
+    rating: float = Field(0, ge=1.0, le=5.0)
+    created_at: datetime = Field(default_factory=datetime.now) 
+    product_id: int 
+    
+class ReviewCreate(ReviewBase): 
+    pass 
+
+class ReviewRead(ReviewBase): 
+    id: int 
+
+class ReviewUpdate(BaseModel): 
+    content: str = Field(None)
+    rating: float = Field(None, ge=1.0, le=5.0)
+
+class OrderBase(BaseModel): 
+    total: float = Field(..., ge=4999.99) 
+    quantity: int = Field(..., gt=1) 
+    shipping_fee: float 
+    shipping_address: str 
+    created_at: datetime = Field(default_factory=datetime.now) 
+    customer_id: int 
+    order_items: list["ItemRead"]
+
+class OrderCreate(OrderBase): 
+    pass 
+
+class OrderRead(OrderBase): 
+    id: int 
+
+class OrderUpdate(BaseModel): 
+    total: float = Field(None, ge=4999.99) 
+    quantity: int = Field(None, gt=1) 
+    shipping_fee: float = Field(None)
+    shipping_address: str = Field(None)
+
+class CategoryBase(BaseModel): 
+    name: Cat 
+
+class CategoryCreate(CategoryBase): 
+    pass 
+
+class CategoryRead(CategoryBase): 
+    id: int 
+
+class CategoryUpdate(BaseModel): 
+    name: Cat = Field(None)
+
+
+class ItemBase(BaseModel): 
+    total: float = Field(..., ge=4999.99) 
+    quantity: int = Field(..., gt=1) 
+    product_id: int 
+    order_id: int 
+
+class ItemCreate(ItemBase): 
+    pass
+
+class ItemRead(ItemBase): 
+    id: int 
+
+class ItemUpdate(BaseModel): 
+    total: float = Field( None, ge=4999.99) 
+    quantity: int = Field (None,  gt=1) 
