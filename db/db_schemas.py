219052901356ea6e4100ef7c4bc147559ff87e9c
Balogun Muhammad Awwal <balogunmuhammadawwal@gmail.com>
2024-12-10 20:57:36 +0100

    chore: update db schema

--- db/db_schemas.py
+++ db/db_schemas.py
@@ -18,7 +18,7 @@ class Cat(Enum):
     SOCK = 'sock'
 
 class Product(Base): 
-    __tablename__='product'
+    __tablename__='products'
     id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
     price: Mapped[float] = mapped_column(Float, nullable=False) 
     discount: Mapped[float] = mapped_column(Float, default=0) 
@@ -34,8 +34,8 @@ class Product(Base):
     reviews: Mapped[List["Review"]] = relationship("Review", cascade='all, delete') 
     
 
-class Customer(Base): 
-    __tablename__='customer'
+class User(Base): 
+    __tablename__='users'
     id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True) 
     firstname: Mapped[str] = mapped_column(String(255), nullable=False) 
     lastname: Mapped[str] = mapped_column(String(255), nullable=False)  
@@ -48,44 +48,44 @@ class Customer(Base):
     
 
 class Review(Base): 
-    __tablename__ = 'review'
+    __tablename__ = 'reviews'
     id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True) 
     content: Mapped[str] = mapped_column(Text, nullable=False)     
     rating: Mapped[float] = mapped_column(Float, nullable=False, max=5.0, min=1.0) 
     created_at: Mapped[datetime] = mapped_column(DateTime, default=datetime.now) 
-    product_id: Mapped[int] = mapped_column(ForeignKey('product.id'), nullable=False) 
+    product_id: Mapped[int] = mapped_column(ForeignKey('products.id'), nullable=False) 
     product: Mapped["Product"] = relationship("Product", back_populates='reviews' ) 
      
 
 class Order(Base): 
-    __tablename__= "order"
+    __tablename__= "orders"
     id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True) 
     total: Mapped[float] = mapped_column(Integer, nullable=False) 
     quantity: Mapped[int] = mapped_column(Integer, default=1) 
     shipping_fee: Mapped[float] = mapped_column(Float, nullable=False) 
     shipping_address: Mapped[float] = mapped_column(Float, nullable=True) 
     created_at: Mapped[datetime] = mapped_column(DateTime, default=datetime.now) 
-    customer_id: Mapped[int] = mapped_column(ForeignKey('customer.id'), nullable=False) 
-    customer: Mapped["Customer"] = relationship("Customer", back_populates="orders") 
+    customer_id: Mapped[int] = mapped_column(ForeignKey('user.id'), nullable=False) 
+    customer: Mapped["User"] = relationship("User", back_populates="orders") 
     order_items: Mapped[List['Item']] = relationship("Item") 
     
 class Cart(Base): 
-    __tablename__= "cart"
+    __tablename__= "carts"
     id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True) 
     total: Mapped[float] = mapped_column(Float, nullable=False) 
     
     created_at: Mapped[datetime] = mapped_column(DateTime, default=datetime.now) 
 
 class Category(Base): 
-    __tablename__= "category"
+    __tablename__= "categorys"
     id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True) 
     name: Mapped[Cat] = mapped_column(Enum(Cat), nullable=False) 
 
 class Item(Base): 
-    __tablename__= "item"
+    __tablename__= "items"
     id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True) 
     total: Mapped[float] = mapped_column(Float, nullable=False ) 
     quantity: Mapped[int] = mapped_column(Integer, default=1) 
-    product_id: Mapped[int] = mapped_column(ForeignKey('product.id'), nullable=False) 
+    product_id: Mapped[int] = mapped_column(ForeignKey('products.id'), nullable=False) 
     product: Mapped[Product] = relationship("Product") 
     order_id: Mapped[int] = mapped_column(ForeignKey('order.id'), nullable=True)
