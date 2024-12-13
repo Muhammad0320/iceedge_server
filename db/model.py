4f2d80ca751733b77d8d29a3daa791182b2d95b9
Balogun Muhammad Awwal <balogunmuhammadawwal@gmail.com>
2024-12-13 20:14:21 +0100

    chore: update model schema

--- db/model.py
+++ db/model.py
@@ -1,7 +1,8 @@
 from sqlalchemy.orm import Mapped, mapped_column, DeclarativeBase, relationship
 from sqlalchemy import DateTime, String, Text, Integer, Float, JSON, Enum, ForeignKey
 from datetime import datetime
-from typing import List
+from typing import List, Optional
+
 class Base(DeclarativeBase):
     pass 
 
@@ -19,43 +20,43 @@ class Cat(Enum):
 
 class Product(Base): 
     __tablename__='products'
-    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
-    price: Mapped[float] = mapped_column(Float, nullable=False) 
-    discount: Mapped[float] = mapped_column(Float, default=0) 
-    name: Mapped[str] = mapped_column(String(255), nullable=False) 
-    description: Mapped[str] = mapped_column(Text, nullable=False) 
-    cat_id: Mapped[int] = mapped_column(ForeignKey('category.id'), nullable=False)
-    thumbnail: Mapped[str] = mapped_column(String(255), nullable=False)
-    gallery: Mapped[list[str]] = mapped_column(JSON, nullable=False) 
-    amt_left: Mapped[int] = mapped_column(Integer, nullable=False)  
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    price: Mapped[float] = mapped_column(Float) 
+    discount: Mapped[float] = mapped_column(Floatdefault=0) 
+    name: Mapped[str] = mapped_column(String(255)) 
+    description: Mapped[str] = mapped_column(Text) 
+    cat_id: Mapped[int] = mapped_column(ForeignKey('category.id'), index=True) 
+    thumbnail: Mapped[str] = mapped_column(String(255))
+    gallery: Mapped[list[str]] = mapped_column(JSON) 
+    amt_left: Mapped[int] = mapped_column(Integer)  
     avg_rating: Mapped[float] = mapped_column(Float, default=0) 
     ratings_count: Mapped[int] = mapped_column(Float, default=0) 
     created_at: Mapped[datetime] = mapped_column(DateTime, default=datetime.now) 
-    reviews: Mapped[List["Review"]] = relationship("Review", cascade='all, delete') 
+    reviews: Mapped[Optional[List["Review"]]] = relationship("Review", cascade='all, delete') 
     
 
 class User(Base): 
     __tablename__='users'
-    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True) 
-    firstname: Mapped[str] = mapped_column(String(255), nullable=False) 
+    id: Mapped[int] = mapped_column(Integer, primary_key=True) 
+    firstname: Mapped[str] = mapped_column(String(255)) 
     role: Mapped[Role] = mapped_column(Enum(Role), default=Role.CUSTOMER) 
-    lastname: Mapped[str] = mapped_column(String(255), nullable=False)  
-    email: Mapped[str] = mapped_column(String(255), nullable=False) 
-    password: Mapped[str] = mapped_column(String(255), nullable=False) 
-    shipping_address: Mapped[str] = mapped_column(Text, nullable=False) 
+    lastname: Mapped[str] = mapped_column(String(255))  
+    email: Mapped[str] = mapped_column(String(255)) 
+    password: Mapped[str] = mapped_column(String(255)) 
+    shipping_address: Mapped[ Optional[str]] = mapped_column(Text) 
     created_at: Mapped[datetime] = mapped_column(DateTime, default=datetime.now) 
-    orders: Mapped[List["Order"]] = relationship("Order", cascade='all, delete')
+    orders: Mapped[ Optional[List["Order"]]] = relationship("Order", cascade='all, delete')
     
 
 class Review(Base): 
     __tablename__ = 'reviews'
-    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True) 
-    content: Mapped[str] = mapped_column(Text, nullable=False)     
-    rating: Mapped[float] = mapped_column(Float, nullable=False, max=5.0, min=1.0) 
+    id: Mapped[int] = mapped_column(Integer, primary_key=True) 
+    content: Mapped[str] = mapped_column(Text)     
+    rating: Mapped[float] = mapped_column(Float, max=5.0, min=1.0) 
     created_at: Mapped[datetime] = mapped_column(DateTime, default=datetime.now) 
-    product_id: Mapped[int] = mapped_column(ForeignKey('products.id'), nullable=False) 
+    product_id: Mapped[int] = mapped_column(ForeignKey('products.id'), index=True)  
     product: Mapped["Product"] = relationship("Product", back_populates='reviews' ) 
-     
+    
 
 class Order(Base): 
     __tablename__= "orders"
