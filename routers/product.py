1005ba62d830c611aea5d6e2c88bcc6d91de64ad
Balogun Muhammad Awwal <balogunmuhammadawwal@gmail.com>
2024-12-13 06:54:09 +0100

    chore: update get products by cat

--- routers/product.py
+++ routers/product.py
@@ -7,9 +7,14 @@ from ..db.db_conn import AsyncSession, get_async_session
 
 router = APIRouter(prefix='/products', tags=['products'])
 
-async def  get_category_by_id(name: Cat, session: AsyncSession = Depends(get_async_session)) -> Category: 
+async def  get_category_by_name(name: Cat, session: AsyncSession = Depends(get_async_session)) -> Category: 
     query = select(Category).where(Category.name == name)
     return (await session.execute(query)).scalar_one() 
+
+async def get_prods_by_cat(cat: Cat) -> list[Product]: 
+    cat_id = (await get_category_by_name(cat)).id 
+    query = select(Product).where(Product.cat_id == cat_id)
+
 
 async def get_products(skip: int = Query(0), limit: int = Query(None, max=100, min=1), session: AsyncSession = Depends(get_async_session)):
     q = select(Product).order_by(Product.created_at)
@@ -18,7 +23,7 @@ async def get_products(skip: int = Query(0), limit: int = Query(None, max=100, m
 @router.post('/', status_code=status.HTTP_201_CREATED,  response_model=ProductRead,responses={ status.HTTP_409_CONFLICT: {"model": Message()} }, 
 )
 async def add_new_prod( new_product: ProductCreate =  Body(example=ProductCreate(name="Product name", price=9999.99,description="product description" ,discount=10, cat=Cat.SHIIRT, thumbnail='product_thumbnail.jpg', amt_left=5, gallery=['second_img.jpg', 'first_img.jpg'], created_at=datetime.now() )), session: AsyncSession = Depends(get_async_session)): 
-    fetched_cat = await get_category_by_id(new_product.cat) 
+    fetched_cat = await get_category_by_name(new_product.cat) 
     product = Product(**new_product.model_dump(exclude_unset=True))
     product.cat_id = fetched_cat.id 
     session.add(product) 
@@ -29,3 +34,6 @@ async def add_new_prod( new_product: ProductCreate =  Body(example=ProductCreate
 async def get_all_products(): 
     products = await get_products()
     return products
+
+
+@router.get('/categories')
