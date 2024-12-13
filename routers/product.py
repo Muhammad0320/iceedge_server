f5dbb31eb4cb18563e3d607a61f188eaf5dcf4e8
Balogun Muhammad Awwal <balogunmuhammadawwal@gmail.com>
2024-12-13 06:39:43 +0100

    chore: add router

--- routers/product.py
+++ routers/product.py
@@ -5,7 +5,7 @@ from ..db.schema import ProductCreate, ProductRead, ProductUpdate, Message, Cat
 from datetime import datetime
 from ..db.db_conn import AsyncSession, get_async_session
 
-router = APIRouter(prefix='/products', tags=['products'], response_model=ProductRead)
+router = APIRouter(prefix='/products', tags=['products'])
 
 async def  get_category_by_id(name: Cat, session: AsyncSession = Depends(get_async_session)) -> Category: 
     query = select(Category).where(Category.name == name)
@@ -15,7 +15,7 @@ async def get_products(skip: int = Query(0), limit: int = Query(None, max=100, m
     q = select(Product).order_by(Product.created_at)
     return (await session.scalars(q)).all() 
 
-@router.post('/', status_code=status.HTTP_201_CREATED, responses={ status.HTTP_409_CONFLICT: {"model": Message()} }, 
+@router.post('/', status_code=status.HTTP_201_CREATED,  response_model=ProductRead,responses={ status.HTTP_409_CONFLICT: {"model": Message()} }, 
 )
 async def add_new_prod( new_product: ProductCreate =  Body(example=ProductCreate(name="Product name", price=9999.99,description="product description" ,discount=10, cat=Cat.SHIIRT, thumbnail='product_thumbnail.jpg', amt_left=5, gallery=['second_img.jpg', 'first_img.jpg'], created_at=datetime.now() )), session: AsyncSession = Depends(get_async_session)): 
     fetched_cat = await get_category_by_id(new_product.cat) 
@@ -27,5 +27,5 @@ async def add_new_prod( new_product: ProductCreate =  Body(example=ProductCreate
 
 @router.get('/', response_model=list[ProductRead])
 async def get_all_products(): 
-    products = get_products()
-    
+    products = await get_products()
+    return products
