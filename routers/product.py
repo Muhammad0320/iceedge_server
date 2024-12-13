dd3c864d4210648ba9108b87bb37ed42c0a9477d
Balogun Muhammad Awwal <balogunmuhammadawwal@gmail.com>
2024-12-13 06:58:05 +0100

    chore:  update prods

--- routers/product.py
+++ routers/product.py
@@ -4,6 +4,7 @@ from ..db.model import Category, Product
 from ..db.schema import ProductCreate, ProductRead, ProductUpdate, Message, Cat
 from datetime import datetime
 from ..db.db_conn import AsyncSession, get_async_session
+from typing import Sequence
 
 router = APIRouter(prefix='/products', tags=['products'])
 
@@ -11,9 +12,10 @@ async def  get_category_by_name(name: Cat, session: AsyncSession = Depends(get_a
     query = select(Category).where(Category.name == name)
     return (await session.execute(query)).scalar_one() 
 
-async def get_prods_by_cat(cat: Cat) -> list[Product]: 
+async def get_prods_by_cat(cat: Cat, session: AsyncSession = Depends(get_async_session)) -> Sequence[Product]: 
     cat_id = (await get_category_by_name(cat)).id 
-    query = select(Product).where(Product.cat_id == cat_id)
+    q = select(Product).where(Product.cat_id == cat_id).order_by(Product.name)
+    return (await session.scalars(q)).all() 
 
 
 async def get_products(skip: int = Query(0), limit: int = Query(None, max=100, min=1), session: AsyncSession = Depends(get_async_session)):
@@ -36,4 +38,7 @@ async def get_all_products():
     return products
 
 
-@router.get('/categories')
+@router.get('/{cat}')
+async def get_products_by_category(cat: Cat = Query(example=Cat.SHIIRT)): 
+    products = await get_prods_by_cat(cat=cat)
+    return products
