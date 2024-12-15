cca22ec4664c8f587bc70b1ebe67f1fff963d054
Balogun Muhammad Awwal <balogunmuhammadawwal@gmail.com>
2024-12-15 20:41:33 +0100

    chore: add new router to the main app

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from contextlib import asynccontextmanager
 from .db.db_conn import create_all_tables
 from .routers import product
+from .routers import review
 
 @asynccontextmanager
 async def lifespan(app: FastAPI): 
@@ -15,3 +16,4 @@ async def welcome():
     return "Welcome to Iceedge, what do you need?"
 
 app.include_router(router=product.router)
+app.include_router(router=review.router)
