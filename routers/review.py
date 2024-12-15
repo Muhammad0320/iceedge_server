30882fc6560d188f188a33b6fbf4948c0aad6f9b
Balogun Muhammad Awwal <balogunmuhammadawwal@gmail.com>
2024-12-15 20:46:34 +0100

    feat: add endpoint to get all reviews associated to a product

--- routers/review.py
+++ routers/review.py
@@ -1,5 +1,12 @@
 from fastapi import APIRouter, Depends, status, Body 
 from ..db.schema import ReviewCreate, ReviewRead, ReviewUpdate
 from ..db.db_conn import AsyncSession, get_async_session
+from sqlalchemy import select, update, delete 
+from .product import get_product_or_404, Product
 
 router = APIRouter(prefix='/review', tags=['reviews'])
+
+@router.get('/product/{id}', response_model=list[ReviewRead] | None)
+async def get_reviews_by_prod(product: Product = Depends(get_product_or_404), session: AsyncSession = Depends(get_async_session)): 
+    return product.reviews
+
