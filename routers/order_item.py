35aa64cdc510313f2e43ad351b61ab31009a4002
Balogun Muhammad Awwal <balogunmuhammadawwal@gmail.com>
2024-12-17 02:55:02 +0100

    feat: add endpoint to get all

--- routers/order_item.py
+++ routers/order_item.py
@@ -17,3 +17,8 @@ async def get_item_or_404(id: int, session: AsyncSession = Depends(get_async_ses
 async def get_item_by_id(item: Item = Depends(get_item_or_404), session: AsyncSession = Depends(get_async_session)):
     return item
 
+# TODO: Only Admins and Developer
+@router.get('/', response_model=list[Item])
+async def get_all_items(session: AsyncSession = Depends(get_async_session)): 
+    return (await session.scalars(select(Item))).all() 
+
