56146e915a3ccd8b9b19b9663476e87408548fc8
Balogun Muhammad Awwal <balogunmuhammadawwal@gmail.com>
2024-12-19 13:42:15 +0100

    feat: add a dependency to get the currently logged in user

--- routers/user.py
+++ routers/user.py
@@ -1,14 +1,15 @@
 from fastapi import APIRouter, Depends, status, HTTPException, Body, Query, Path
-from sqlalchemy import select, update, delete, func
+from sqlalchemy import select, and_
 from sqlalchemy.orm import joinedload
-from ..db.model import User
+from ..db.model import User, AccessToken
 from sqlalchemy.exc import IntegrityError
 from fastapi.security import OAuth2PasswordRequestForm, OAuth2PasswordBearer
 from ..security.password import hash_password
 from ..db.schema import UserCreate, Message, UserRead, Credential
-from datetime import datetime
+from datetime import datetime, timezone
 from ..db.db_conn import AsyncSession, get_async_session
 from ..security.authenticate import authenticate, create_access_token
+
 
 router = APIRouter(prefix='/user', tags=['users', 'all']) 
 
@@ -23,7 +24,6 @@ async def register_user(user: UserCreate, session: AsyncSession = Depends(get_as
         raise HTTPException(status_code=status.HTTP_409_CONFLICT)  
     return updated_object
 
-
 @router.post('/token', response_model=UserRead)
 async def signin(credential: OAuth2PasswordRequestForm = Depends(OAuth2PasswordRequestForm), session: AsyncSession = Depends(get_async_session)): 
     email = credential.username; password = credential.password
@@ -32,4 +32,11 @@ async def signin(credential: OAuth2PasswordRequestForm = Depends(OAuth2PasswordR
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid signin credentials")
     token = await create_access_token(user, session) 
     return {"access_token": token.token, "token_type": "bearer"}
+
+async def get_current_user(token_str: str = Depends(OAuth2PasswordBearer(tokenUrl='/token')), session: AsyncSession = Depends(get_async_session)):
+    q = select(AccessToken).where(and_(AccessToken.token == token_str, AccessToken.expiration_date == datetime.now(tz=timezone.utc)))
+    token = (await session.scalars(q)).one_or_none() 
+    if not token: 
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED) 
+    return token.user 
 
