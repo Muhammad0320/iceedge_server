360f6215318f53d43f424848e05158a44b141cc4
Balogun Muhammad Awwal <balogunmuhammadawwal@gmail.com>
2024-12-22 15:09:52 +0100

    chore: add another dependency

--- dependencies.py
+++ dependencies.py
@@ -1,9 +1,11 @@
 from .db.model import AccessToken
 from fastapi.security import OAuth2PasswordBearer
 from sqlalchemy import select, and_ 
+from fastapi.security import APIKeyCookie
 from fastapi import Depends, status, HTTPException
 from .db.db_conn import AsyncSession, get_async_session
 from datetime import timezone, datetime
+from .main import TOKEN_COOKIE_NAME
 
 async def get_current_user_by_token(token_str: str = Depends(OAuth2PasswordBearer(tokenUrl='/token')), session: AsyncSession = Depends(get_async_session)):
     q = select(AccessToken).where(and_(AccessToken.token == token_str, AccessToken.expiration_date == datetime.now(tz=timezone.utc)))
@@ -11,3 +13,7 @@ async def get_current_user_by_token(token_str: str = Depends(OAuth2PasswordBeare
     if not token: 
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED) 
     return token.user 
+
+async def get_curr_user(token: str = Depends(APIKeyCookie(name=TOKEN_COOKIE_NAME))): 
+    user = await get_current_user_by_token(token_str=token) 
+    return user  
