68c7b21be8667c05106edff738c5e78f1f9fbf6f
Balogun Muhammad Awwal <balogunmuhammadawwal@gmail.com>
2024-12-24 21:39:45 +0100

    feat: add test coverage for test_products

--- tests/test_products.py
+++ tests/test_products.py
@@ -23,8 +23,8 @@ class TestCreateProduct:
         assert res.status_code == status.HTTP_422_UNPROCESSABLE_ENTITY
     
     async def test_create_invalid_fields(self, test_client: httpx.AsyncClient): 
-        app.dependency_overrides[get_curr_user] = get_fake_user_by_role(Role.DEVELOPER)
-        new_prod = ProductCreate(name='Black Hoodie', price=9999, discount=5, thumbnail='thumbnail.png', gallery=['gallery_img_1.png', 'gallery_img_2.png'], amt_left=10, cat=Cat.SHIRT )
+        app.dependency_overrides[get_curr_user] = TestUser(Role.DEVELOPER).get_fake_user
+        new_prod = {'name':'Black Hoodie', 'price':9999, 'discount':5, 'thumbnail':'thumbnail.png', 'gallery':['gallery_img_1.png', 'gallery_img_2.png'], 'amt_left':10, 'cat':Cat.SHIRT}
         res = await test_client.post('/products/', json=new_prod) 
         assert res.status_code == status.HTTP_422_UNPROCESSABLE_ENTITY
     
