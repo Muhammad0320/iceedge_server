f2b54933b78696e5740391295e7b6a718f632859
Balogun Muhammad Awwal <balogunmuhammadawwal@gmail.com>
2024-12-25 21:20:40 +0100

    chore: create a utility function to create new prod

--- tests/test_products.py
+++ tests/test_products.py
@@ -7,6 +7,17 @@ from ..main import app
 from ..routers.product import get_curr_user, Product, Cat
 
 
+async def create_new_prod(test_client: httpx.AsyncClient): 
+        new_prod = {
+            'name':'Black Hoodie', 'price':9999, 'discount':5, 'thumbnail':'thumbnail.png', 'gallery':['gallery_img_1.png', 'gallery_img_2.png'], 'amt_left':10, 'cat':Cat.SHIRT, 'description':'Beautiful Hoodie'
+        } 
+        product = await test_client.post('/products/', json=new_prod) 
+        assert product.status_code == status.HTTP_201_CREATED
+        data: Product | None = product.json() 
+        if not data: 
+            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST)
+        return data
+
 @pytest.mark.asyncio 
 class TestCreateProduct: 
     
@@ -38,6 +49,12 @@ class TestCreateProduct:
         if data: 
             assert data.name == new_prod['name']
  
+
+@pytest.mark.asyncio
+async def test_get_all_products(test_client: httpx.AsyncClient): 
+    res = await test_client.get('/products')
+    
+
 @pytest.mark.asyncio   
 class TestGetProduct:
     async def test_get_product_invalid_format(self, test_client: httpx.AsyncClient): 
@@ -50,15 +67,6 @@ class TestGetProduct:
     
     async def test_get_product(self, test_client: httpx.AsyncClient): 
         app.dependency_overrides[get_curr_user] = TestUser(Role.DEVELOPER).get_fake_user
-        new_prod = {
-            'name':'Black Hoodie', 'price':9999, 'discount':5, 'thumbnail':'thumbnail.png', 'gallery':['gallery_img_1.png', 'gallery_img_2.png'], 'amt_left':10, 'cat':Cat.SHIRT, 'description':'Beautiful Hoodie'
-        } 
-        product = await test_client.post('/products/', json=new_prod) 
-        assert product.status_code == status.HTTP_201_CREATED
-        data: Product | None = product.json() 
-        if not data: 
-            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST)
         res = await test_client.get(f'/products/{data.id}')
         res.status_code == status.HTTP_200_OK
-            
-    
+
