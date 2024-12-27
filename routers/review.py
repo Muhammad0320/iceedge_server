f302268d494ff39a10ba2982945641b4dd9a14f2
Balogun Muhammad Awwal <balogunmuhammadawwal@gmail.com>
2024-12-27 21:14:58 +0100

    chore: update review.py

--- routers/review.py
+++ routers/review.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, Depends, status, Body, HTTPException, Path
 from ..db.schema import ReviewCreate, ReviewRead, ReviewUpdate
 from ..db.db_conn import AsyncSession, get_async_session
 from ..db.model import Review, Product
-from sqlalchemy import select, update, delete 
+from sqlalchemy import select, update, delete, func 
 from .product import get_product_or_404
 
 router = APIRouter(prefix='/review', tags=['reviews'])
@@ -12,9 +12,15 @@ async def get_review_or_404(id: int, session: AsyncSession = Depends(get_async_s
     if not result: 
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Review you requested is not found!')
     return result
-@router.get('/product/{id}', response_model=list[ReviewRead] | None)
-async def get_reviews_by_prod(product: Product = Depends(get_product_or_404)): 
-    return product.reviews
+
+@router.get('/product/{id}')
+async def get_reviews_by_prod(id: int, session: AsyncSession = Depends(get_async_session)): 
+    if not (await get_product_or_404(id, session)):
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Product not found")
+    review_count = func.count(Review.id).label(None) 
+    q = select(Product.name, Review, review_count).where(Product.id == id).join(Product.reviews).order_by(Review.created_at, Product.name) 
+    res = (await session.scalars(q)).all()
+    return res 
 
 @router.post('/', response_model=ReviewRead, status_code=status.HTTP_201_CREATED)
 async def create_post(new_review: ReviewCreate = Body(example=ReviewCreate(content="The best", rating=5.0, product_id=999)), session: AsyncSession = Depends(get_async_session)): 
