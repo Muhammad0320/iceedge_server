627f501f35ca3170b149895c887e3ed5980a8964
Balogun Muhammad Awwal <balogunmuhammadawwal@gmail.com>
2024-12-31 21:50:30 +0100

    chore: update test order

--- tests/test_order.py
+++ tests/test_order.py
@@ -38,8 +38,8 @@ customer_id=uuid4()
 @pytest_asyncio.fixture(scope='module')
 async def create_test_order(product: Product ,test_client: httpx.AsyncClient) -> Order: 
     app.dependency_overrides[get_curr_user] = TestUser(Role.DEVELOPER, customer_id).get_fake_user
-    order = Order(total=500, quantity=10, shipping_fee=50, shipping_address='G50', customer_id=customer_id, order_item=[order_item])
-    order_item = OrderItem(unit_price=100, quantity=5, product_id=product.id, order_id=order.id)
+    order = Order(total=500, quantity=10, shipping_fee=50, shipping_address='G50', customer_id=customer_id, order_item=['Order_item'])
+    Order_item = OrderItem(unit_price=100, quantity=5, product_id=product.id, order_id=order.id)
     await test_client.post('/order', json=order)
     return order
 
@@ -52,7 +52,13 @@ class TestGetOneOrder:
         assert result.status_code == status.HTTP_401_UNAUTHORIZED
     
     async def test_not_found(self, test_client: httpx.AsyncClient): 
+        app.dependency_overrides[get_curr_user] = TestUser(Role.CUSTOMER).get_fake_user
         result = await test_client.get(self.url)
         assert result.status_code == status.HTTP_404_NOT_FOUND
     
-    async def test_valid
+    async def test_valid(self, test_client: httpx.AsyncClient, order: Order): 
+        app.dependency_overrides[get_curr_user] = TestUser(Role.CUSTOMER).get_fake_user
+        result = await test_client.get(f"{self.url}{order.id}")
+        assert result.status_code == status.HTTP_200_OK
+    
+    
