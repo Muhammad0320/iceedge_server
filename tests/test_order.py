7ee4bf9997ec717f6f7cfc52dc097bf7667afc89
Balogun Muhammad Awwal <balogunmuhammadawwal@gmail.com>
2024-12-31 05:42:53 +0100

    test: add one test class

--- tests/test_order.py
+++ tests/test_order.py
@@ -9,3 +9,12 @@ from ..db.model import User
 from ..db.schema import UserCreate, UserUpdate, OrderCreate, OrderRead
 from ..routers.product import get_curr_user, Product, Cat
 from uuid import uuid4
+
+class TestGetOneOrder: 
+    def __init__(self): 
+        self.url = '/orders/'
+
+    async def test_get_all(self, test_client: httpx.AsyncClient): 
+        result = await test_client.get(self.url)
+        assert result.status_code == status.HTTP_401_UNAUTHORIZED
+    
